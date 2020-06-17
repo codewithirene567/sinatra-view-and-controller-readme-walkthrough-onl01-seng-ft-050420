a9e795ed9d03b7214f705b38df55706e93f4608f
Irene Scott <irenejinscott@gmail.com>
2020-06-17 20:07:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,8 +6,9 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    original_string = params["string"]
-    @reversed_string = original_string.reverse
+    original_string = params["string"] #call whatever string they put in there and call it an original
+    #string and make the string equal to a key called string
+    @reversed_string = original_string.reverse #now set a variable of the original string in reverse equal to @reversed string for every instance
 
     erb :reversed
   end
